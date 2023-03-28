190d5d66b92d58078149df046c0bfa8dc48b0ef3
Edy Segura <edysegura@gmail.com>
2023-03-27 21:24:53 -0300

    first commit

---
+++ force-app/main/default/lwc/numerator/numerator.js
@@ -0,0 +1,14 @@
+import { LightningElement } from "lwc";
+
+export default class Numerator extends LightningElement {
+  counter = 0;
+
+  handleIncrement() {
+    this.counter++;
+  }
+
+  handleDecrement() {
+    this.counter--;
+  }
+
+}
